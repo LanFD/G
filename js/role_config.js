792545279c775024d1eae11a5415dc71d32d0dc4
LanFD <382354412@qq.com>
2016-09-12 15:23:10 +0800

    css

---
+++ js/role_config.js
@@ -0,0 +1,12 @@
+var roles;
+var rolePath = 'role/';
+roles        = {
+    firstRole: {
+        name: 'Atia',
+        path: rolePath + 'role1'
+    },
+    secondRole: {
+        name: 'assassin',
+        path: rolePath + 'role2'
+    }
+}
